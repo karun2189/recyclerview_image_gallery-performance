2eeeec553ce98e7d69d9ec50e570fe3236c21396
arun <arunkumar.kesavan@zohocorp.com>
2015-08-08 14:28:10 +0530

    Initial commit

---
+++ app/src/main/java/nigelhenshaw/com/cameraintenttutorial/CamaraIntentActivity.java
@@ -0,0 +1,233 @@
+package nigelhenshaw.com.cameraintenttutorial;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.LruCache;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.ref.SoftReference;
+import java.text.SimpleDateFormat;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+
+public class CamaraIntentActivity extends Activity {
+
+    private static final int ACTIVITY_START_CAMERA_APP = 0;
+    private ImageView mPhotoCapturedImageView;
+    private String mImageFileLocation = "";
+    private String GALLERY_LOCATION = "image gallery";
+    private File mGalleryFolder;
+    private static LruCache<String, Bitmap> mMemoryCache;
+    private RecyclerView mRecyclerView;
+    private static Set<SoftReference<Bitmap>> mReuseableBitmap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camara_intent);
+
+        createImageGallery();
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.galleryRecyclerView);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(this, 2);//LayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
+        RecyclerView.Adapter imageAdapter = new ImageAdapter(mGalleryFolder);
+        mRecyclerView.setAdapter(imageAdapter);
+
+        final int maxMemorySize = (int) Runtime.getRuntime().maxMemory() / 1024;
+        final int cacheSize = maxMemorySize / 100;
+        // final int cacheSize = maxMemorySize / 10;
+
+        mMemoryCache = new LruCache<String, Bitmap>(cacheSize) {
+
+            @Override
+            protected void entryRemoved(boolean evicted, String key, Bitmap oldValue, Bitmap newValue) {
+                super.entryRemoved(evicted, key, oldValue, newValue);
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR1) {
+                    mReuseableBitmap.add(new SoftReference<Bitmap>(oldValue));
+                }
+            }
+
+            @Override
+            protected int sizeOf(String key, Bitmap value) {
+                return value.getByteCount() / 1024;
+            }
+        };
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR1) {
+            mReuseableBitmap = Collections.synchronizedSet(new HashSet<SoftReference<Bitmap>>());
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_camara_intent, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void takePhoto(View view) {
+        Intent callCameraApplicationIntent = new Intent();
+        callCameraApplicationIntent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        File photoFile = null;
+        try {
+           photoFile = createImageFile();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        callCameraApplicationIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+
+        startActivityForResult(callCameraApplicationIntent, ACTIVITY_START_CAMERA_APP);
+    }
+
+    protected void onActivityResult (int requestCode, int resultCode, Intent data) {
+        if(requestCode == ACTIVITY_START_CAMERA_APP && resultCode == RESULT_OK) {
+            // Toast.makeText(this, "Picture taken successfully", Toast.LENGTH_SHORT).show();
+            // Bundle extras = data.getExtras();
+            // Bitmap photoCapturedBitmap = (Bitmap) extras.get("data");
+            // mPhotoCapturedImageView.setImageBitmap(photoCapturedBitmap);
+            // Bitmap photoCapturedBitmap = BitmapFactory.decodeFile(mImageFileLocation);
+            // mPhotoCapturedImageView.setImageBitmap(photoCapturedBitmap);
+            // setReducedImageSize();
+            RecyclerView.Adapter newImageAdapter = new ImageAdapter(mGalleryFolder);
+            mRecyclerView.swapAdapter(newImageAdapter, false);
+
+        }
+    }
+
+    private void createImageGallery() {
+        File storageDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        mGalleryFolder = new File(storageDirectory, GALLERY_LOCATION);
+        if(!mGalleryFolder.exists()) {
+            mGalleryFolder.mkdirs();
+        }
+
+    }
+
+    File createImageFile() throws IOException {
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "IMAGE_" + timeStamp + "_";
+
+        File image = File.createTempFile(imageFileName,".jpg", mGalleryFolder);
+        mImageFileLocation = image.getAbsolutePath();
+
+        return image;
+
+    }
+
+    void setReducedImageSize() {
+        int targetImageViewWidth = mPhotoCapturedImageView.getWidth();
+        int targetImageViewHeight = mPhotoCapturedImageView.getHeight();
+
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(mImageFileLocation, bmOptions);
+        int cameraImageWidth = bmOptions.outWidth;
+        int cameraImageHeight = bmOptions.outHeight;
+
+        int scaleFactor = Math.min(cameraImageWidth/targetImageViewWidth, cameraImageHeight/targetImageViewHeight);
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inJustDecodeBounds = false;
+
+        Bitmap photoReducedSizeBitmp = BitmapFactory.decodeFile(mImageFileLocation, bmOptions);
+        mPhotoCapturedImageView.setImageBitmap(photoReducedSizeBitmp);
+
+
+    }
+
+    public static Bitmap getBitmapFromMemoryCache(String key) {
+        return mMemoryCache.get(key);
+    }
+
+    public static void setBitmapToMemoryCache(String key, Bitmap bitmap) {
+        if(getBitmapFromMemoryCache(key) == null) {
+            mMemoryCache.put(key, bitmap);
+        }
+    }
+
+    private static int getBytesPerPixel(Bitmap.Config config) {
+        if(config == Bitmap.Config.ARGB_8888) {
+            return 4;
+        } else if(config == Bitmap.Config.RGB_565) {
+            return 2;
+        } else if(config == Bitmap.Config.ARGB_4444) {
+            return 2;
+        } else if(config == Bitmap.Config.ALPHA_8) {
+            return 1;
+        }
+        return 1;
+    }
+
+    private static boolean canUseForBitmap(Bitmap candidate, BitmapFactory.Options options) {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            int width = options.outWidth / options.inSampleSize;
+            int height = options.outHeight / options.inSampleSize;
+            int byteCount = width * height * getBytesPerPixel(candidate.getConfig());
+            return byteCount <= candidate.getAllocationByteCount();
+        }
+        return candidate.getWidth() == options.outWidth &&
+                candidate.getHeight() == options.outHeight &&
+                options.inSampleSize == 1;
+    }
+
+    public static Bitmap getBitmapFromReuseableSet(BitmapFactory.Options options) {
+        Bitmap bitmap = null;
+        if(mReuseableBitmap != null && !mReuseableBitmap.isEmpty()) {
+            synchronized (mReuseableBitmap) {
+                Bitmap item;
+                Iterator<SoftReference<Bitmap>> iterator = mReuseableBitmap.iterator();
+                while(iterator.hasNext()) {
+                    item = iterator.next().get();
+                    if(item != null && item.isMutable()) {
+                        if(canUseForBitmap(item, options)) {
+                            bitmap = item;
+                            iterator.remove();
+                            break;
+                        }
+                    } else {
+                        iterator.remove();
+                    }
+                }
+            }
+        }
+        return bitmap;
+    }
+}
